650e3ad35452aa6909ba1d053a44265781e1adf9
wtnerb <wtnerb@gmail.com>
2018-03-28 19:50:03 -0700

    reorg as per class mention

---
+++ Data Structures/LinkedLists/llKthFromEnd/llKthFromEnd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kth_from_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList l = new LinkedList(new int[] { 1, 5, 3, 6, 7 });
+            Node result0 = l.KthFromEnd(0);
+            Node expect0 = new Node(1);
+            Node result1 = l.KthFromEnd(1);
+            Node expect1 = new Node(5);
+            expect1.Next = new Node(1);
+            Console.WriteLine(expect0 == result0); 
+            Console.ReadKey();
+        }
+    }
+}
