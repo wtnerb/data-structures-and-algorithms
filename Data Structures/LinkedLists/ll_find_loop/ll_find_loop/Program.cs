ba40ff2186f2a13d22322420ee2386ede6ce8cfc
wtnerb <wtnerb@gmail.com>
2018-04-02 18:59:49 -0700

    Writes to console.

--- Data Structures/LinkedLists/ll_find_loop/ll_find_loop/Program.cs
+++ Data Structures/LinkedLists/ll_find_loop/ll_find_loop/Program.cs
@@ -7,6 +7,33 @@ namespace ll_find_loop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("input non circular loop");
+            LinkedList ll1 = new LinkedList(new int[] { 1, 5, 2, 83, 2 });
+            RenderLL(ll1);
+            Console.WriteLine($"Is a loop: {ll1.HasLoop()}");
+            Console.WriteLine("\nNow making tail point to head ...");
+            Node current = ll1.Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = ll1.Head;
+            Console.WriteLine("Linked list now ouroboros-ified");
+            Console.WriteLine($"Is a loop: {ll1.HasLoop()}");
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+
+
+        public static void RenderLL(LinkedList ll)
+        {
+            Node current = ll.Head;
+            while (current != null)
+            {
+                Console.Write($"{current.Value} --> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
         }
     }
 }
