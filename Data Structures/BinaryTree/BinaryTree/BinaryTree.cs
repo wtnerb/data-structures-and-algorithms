e898c0952754c04151f52e07f1de3ba40b3d9009
wtnerb <wtnerb@gmail.com>
2018-04-11 10:08:55 -0700

    everything but testing

--- Data Structures/BinaryTree/BinaryTree/BinaryTree.cs
+++ Data Structures/BinaryTree/BinaryTree/BinaryTree.cs
@@ -8,15 +8,99 @@ namespace BinaryTree
     {
         public Node Root { get; set; }
 
-        public void Render(string name)
+        /// <summary>
+        /// Recursively traverses the tree. In a Binary Search tree, this will return the nodes in order.
+        /// </summary>
+        /// <param name="source">the current node in the recursive traversal</param>
+        public void InorderTraverse(Node source)
         {
-            Console.WriteLine($"This is the {name} tree");
+            if (source.Left != null)
+            {
+                InorderTraverse(source.Left);
+            }
+            Console.WriteLine(source.Value);
+            if (source.Right != null)
+            {
+                InorderTraverse(source.Right);
+            }
+        }
+
+        //Overload starts traverse at root of tree to begin recursive traversal
+        public void InorderTraverse()
+        {
+            InorderTraverse(Root);
+        }
+
+        public void PreorderTraverse (Node source)
+        {
+            Console.WriteLine(source.Value);
+            if (source.Left != null)
+            {
+                PreorderTraverse(source.Left);
+            }
+            if (source.Right != null)
+            {
+                PreorderTraverse(source.Right);
+            }
+        }
+
+        public void PreorderTraverse()
+        {
+            PreorderTraverse(Root);
+        }
+
+        public void PostorderTraverse(Node source)
+        {
+            Console.WriteLine(source.Value);
+            if (source.Left != null)
+            {
+                PostorderTraverse(source.Left);
+            }
+            if (source.Right != null)
+            {
+                PostorderTraverse(source.Right);
+            }
+        }
+
+        public void PostorderTraverse()
+        {
+            PreorderTraverse(Root);
+        }
+
+
+        public void Add(int value)
+        {
+            Node insertion = new Node
+            {
+                Value = value,
+                Right = null,
+                Left = null
+            };
             Queue<Node> q = new Queue<Node>();
             q.Enqueue(Root);
-            while (q.Peek() = null)
+            while (q.Peek() != null)
             {
-
+                Node temp = q.Dequeue();
+                if (temp.Left != null)
+                {
+                    q.Enqueue(temp.Left);
+                }
+                else
+                {
+                    temp.Left = insertion;
+                    return;
+                }
+                if (temp.Right != null)
+                {
+                    q.Enqueue(temp.Right);
+                }
+                else
+                {
+                    temp.Right = insertion;
+                    return;
+                }
             }
+
         }
     }
 }
--- Data Structures/BinaryTree/BinaryTree/Program.cs
+++ Data Structures/BinaryTree/BinaryTree/Program.cs
@@ -6,7 +6,28 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, building a simple tree.");
+            Console.WriteLine(
+                "     |6| -- the root\n" +
+                "     /  \\ \n" +
+                "  |4|   |300| \n" +
+                "  / \\     /  \\ \n" +
+                "|-5| |5| |25| |443| ");
+            BinaryTree tree = new BinaryTree();
+            tree.Root = new Node() { Value = 6, Left = null, Right = null};
+            int[] data = new int[] { 4, 300, -5, 5, 25, 443 };
+            foreach (int n in data)
+            {
+                tree.Add(n);
+            }
+            Console.WriteLine("Should be built. Testing: print in order:");
+            tree.InorderTraverse();
+            Console.WriteLine("should be built. Testing, print using post order");
+            tree.PostorderTraverse();
+            Console.WriteLine("Should be built. Testing, print using pre order");
+            tree.PreorderTraverse();
+            Console.ReadKey();
+
         }
     }
 }
