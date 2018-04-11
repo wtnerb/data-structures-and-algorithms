46ba6c2947639967546f5e4cad0f1c427453e1ea
wtnerb <wtnerb@gmail.com>
2018-04-11 09:14:28 -0700

    Basics and scaffold

---
+++ Data Structures/BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public void Render(string name)
+        {
+            Console.WriteLine($"This is the {name} tree");
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(Root);
+            while (q.Peek() = null)
+            {
+
+            }
+        }
+    }
+}
