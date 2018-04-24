66a8ad2b8233a2ceb737c624cda8fc538ff786ec
wtnerb <wtnerb@gmail.com>
2018-04-24 13:33:37 -0700

    initial tools

---
+++ Data Structures/K-aryTrees/K-aryTrees/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaryTrees
+{
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public List<Node<T>> Children { get; set; }
+    }
+}
---
+++ Data Structures/K-aryTrees/K-aryTrees/Tree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace KaryTrees
+{
+    class Tree<T>
+    {
+        public Tree(Node<T> root)
+        {
+            Root = root;
+        }
+
+        public Node<T> Root { get; set; }
+
+        public delegate void Method(Node<T> n);
+
+        public void PreOrderTraverse(Method func)
+        {
+            func(Root);
+            foreach (Node<T> n in Root.Children)
+            {
+                Tree<T> t = new Tree<T>(n);
+                t.PreOrderTraverse(func);
+            }
+        }
+
+        public void PostOrderTraverse(Method func)
+        {
+            foreach (Node<T> n in Root.Children)
+            {
+                Tree<T> t = new Tree<T>(n);
+                t.PreOrderTraverse(func);
+            }
+            func(Root);
+        }
+
+        public void BreadthFirstTraverse(Method func)
+        {
+            Queue<Node<T>> q = new Queue<Node<T>>();
+            q.Enqueue(Root);
+            Node<T> current;
+            while (q.TryDequeue(out current))
+            {
+                func(current);
+                foreach (Node<T> n in current.Children)
+                {
+                    q.Enqueue(n);
+                }
+            }
+        }
+
+        public void Add(T target, T addition)
+        {
+            bool added = false;
+            void AddConditionally(Node<T> current)
+            {
+                if (!added && current.Value.Equals(target))
+                {
+                    current.Children.Add(new Node<T> { Value = addition });
+                }
+            }
+            BreadthFirstTraverse(AddConditionally);
+        }
+    }
+}
