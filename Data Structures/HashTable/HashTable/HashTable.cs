0e1cad520b668282d361f58c5625379ba36d18ff
wtnerb <wtnerb@gmail.com>
2018-05-09 22:35:24 -0700

    scaffold

---
+++ Data Structures/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class HashTable<T>
+    {
+        public Node<T>[] Map { get; set; }
+
+        public HashTable()
+        {
+            Map = new Node<T>[5000];
+        }
+
+        public int Hash(T val)
+        {
+            int n = 0;
+            long num = 0;
+            if (val is string)
+            {
+                foreach (char c in val.ToString())
+                {
+                    n++;
+                    num += c - 90 + F(n);
+                }
+            }
+            if (val is int)
+            {
+                num = (int)val;
+            }
+
+            return (int)num % Map.Length;
+        }
+
+        public int F(int number)
+        {
+            
+        }
+    }
+}
---
+++ Data Structures/HashTable/HashTable/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
