3848c195fd44f924c53490a9a91e4b5e725a73bd
wtnerb <wtnerb@gmail.com>
2018-11-10 21:10:42 -0800

    build heap test fails

--- Data Structures/MaxHeap/HeapTesting/UnitTest1.cs
+++ Data Structures/MaxHeap/HeapTesting/UnitTest1.cs
@@ -6,10 +6,21 @@ namespace HeapTesting
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        //Proof of life
+        public void CanStartHeap()
         {
             Max_Heap mh = new Max_Heap(new int[]{5});
             Assert.True(mh.Root.Val == 5);
+        }
+
+        [Fact]
+        public void CanBuildHeap(){
+            Max_Heap mh = new Max_Heap(new int[]{5, 4, 3, 2, 1});
+            Assert.True(mh.Root.Val == 5);
+            Assert.True(mh.Root.Left.Val == 4);
+            Assert.True(mh.Root.Right.Val == 3);
+            Assert.True(mh.Root.Right.Left.Val == 2);
+            Assert.True(mh.Root.Right.Right.Val == 1);
         }
     }
 }
